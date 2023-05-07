f953e56db7c63d1ee5e8a2cff2c387928c61cb5e
TessyJames28 <tessyjames28@gmail.com>
2023-05-07 19:04:15 +0100

    improved the filter function

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,24 +1,65 @@
 // Adds and handle new attributes in the amenities section for checkbox implementation
 $('document').ready(function () {
+  function buildArticle(response) {
+    $('SECTION.places').empty()
+    response.forEach(place => {
+        const name = place.name;
+        const desc = place.description;
+        const guests = place.max_guest ? `${place.max_guest} Guests` : `${place.max_guest} Guest`;
+        const baths = place.number_bathrooms ? `${place.number_bathrooms} Bathrooms` : `${place.number_bathrooms} Bathroom`;
+        const rooms = place.number_rooms ? `${place.number_rooms} Bedrooms` : `${place.number_rooms} Bedroom`;
+        const price = `$${place.price_by_night}`;
+
+        $('SECTION.places').append(`\
+                                <article>
+                                <div class="title_box">
+                                <h2>${name}</h2>
+                                <div class="price_by_night">${price}</div>
+                                </div>
+                                <div class="information">
+                                <div class="max_guest">${guests}</div>
+                                <div class="number_rooms">${rooms}</div>
+                                <div class="number_bathrooms">${baths}</div>
+                                </div>
+                                <div class="user">
+                                <b>Owner:</b> John Doe
+                                </div>
+                                <div class="description">
+                                ${desc}
+                                </div>
+                                </article>`);
+      });
+  }
   const amenities = {};
   let amenitiesList = '';
+  let amenitiesIdList = [];
   $('input[type="checkbox"]').change(function () {
     const amenityId = $(this).attr('data-id');
     const amenityName = $(this).attr('data-name');
 
     if ($(this).is(':checked')) {
       amenities[amenityId] = amenityName;
+      amenitiesIdList.push(amenityId);
     } else {
       delete amenities[amenityId];
+      amenitiesIdList.filter(amenId => amenityId !== amenId);
     }
 
+    //$('.amenities h4').html('&nbsp;');
     amenitiesList = Object.values(amenities).join(',');
+    if (amenitiesList.length === 0) {
+      $('.amenities h4').text();
+    } else if (amenitiesList.length > 30) {
+      amenitiesList = amenitiesList.slice(0, 30) + '...';
+      $('.amenities h4').text(amenitiesList);
+    } else {
     $('.amenities h4').text(amenitiesList);
+    }
   });
 
   // handles DIV#api_status id in the template to add or remove Class based on status response
   const statusId = $('DIV#api_status');
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (response) {
+  $.get('http://127.0.0.1:5001/api/v1/status/', function (response) {
     if (response.status === 'OK') {
       console.log('OK');
       statusId.addClass('available');
@@ -32,37 +73,11 @@ $('document').ready(function () {
   // Task 4 - Handles Places search by recreating the Article verrsion of the template to load with jQuery POST method
   $.ajax({
     type: 'POST',
-  	url: 'http://0.0.0.0:5001/api/v1/places_search/',
+  	url: 'http://127.0.0.1:5001/api/v1/places_search/',
   	data: '{}',
     contentType: 'application/json',
     success: function (response) {
-      response.forEach(place => {
-        const name = place.name;
-        		const desc = place.description;
-        		const guests = place.max_guest ? `${place.max_guest} Guests` : `${place.max_guest} Guest`;
-        		const baths = place.number_bathrooms ? `${place.number_bathrooms} Bathrooms` : `${place.number_bathrooms} Bathroom`;
-        		const rooms = place.number_rooms ? `${place.number_rooms} Bedrooms` : `${place.number_rooms} Bedroom`;
-        		const price = `$${place.price_by_night}`;
-
-        $('SECTION.places').append(`\
-				<article>
-				<div class="title_box">
-				<h2>${name}</h2>
-				<div class="price_by_night">${price}</div>
-				</div>
-				<div class="information">
-				<div class="max_guest">${guests}</div>
-				<div class="number_rooms">${rooms}</div>
-				<div class="number_bathrooms">${baths}</div>
-				</div>
-				<div class="user">
-				<b>Owner:</b> John Doe
-				</div>
-				<div class="description">
-				${desc}
-				</div>
-				</article>`);
-      });
+      buildArticle(response);
     },
       	error: function (xhr, status, error) {
       		console.log(error);
@@ -74,10 +89,12 @@ $('document').ready(function () {
   $('button').click(function() {
     $.ajax({
       type: 'POST',
-      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      url: 'http://127.0.0.1:5001/api/v1/places_search',
       contentType: 'application/json',
-      data: JSON.stringify({ amenities: amenitiesList }),
+      data: JSON.stringify({ amenities: amenitiesIdList }),
       success: function(response) {
+	buildArticle(response);
+        console.log(amenitiesIdList);
         console.log(response);
       },
       error: function(xhr, status, error) {
